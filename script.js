5c0225b394e72de01fb22b8b5909350dadc3e9f6
Avery St John <averywstjohn1@gmail.com>
2024-10-22 00:46:37 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+/* This function "renders" some bit of text as audio to the user. */
+function renderTTS(text) {
+  var synthesis = window.speechSynthesis;
+
+  console.log("Available Voices:");
+  var voice = synthesis.getVoices().filter(function (voice) {
+    console.log(voice.name + " " + voice.lang);
+    return voice.name === 'Google UK English Female';
+  })[0];
+
+  // Create an utterance object
+  var utterance = new SpeechSynthesisUtterance(text);
+
+  // Set utterance properties
+  utterance.voice = voice;
+  utterance.pitch = 1;
+  utterance.rate = 1;
+  // utterance.volume = 1;
+
+  var volume = parseFloat(document.getElementById("volume").value);
+  utterance.volume = volume;
+  
+  // Speak the utterance
+  synthesis.speak(utterance);
+}
+
+function encourage() {
+  let name = document.getElementById("name").value;
+  let encouragement = document.getElementById("encouragement").value;
+  let postScript = document.getElementById("post-script").value;
+  let message = `Hello ${name}, I have something I would like to tell you. ${encouragement} P.S. ${postScript} Have a nice day!`
+  renderTTS(message);
+}
